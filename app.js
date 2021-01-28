8af66d3d055cb0c7fc7da158a35f241b85398e3b
Ivan Afandi <ivan@e8growth.com>
2021-01-28 16:20:47 +0700

    change pen size

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var num = Math.floor(Math.random() * 2) +1;
 
 $('#card').wScratchPad({
-    size: 100,          // The size of the brush/scratch.
+    size: 50,          // The size of the brush/scratch.
     bg: `image/sc-win ${num}.png`,  // Background (image path or hex color).
     fg: 'image/sc-default.png',  // Foreground (image path or hex color).
     realtime: true,       // Calculates percentage in realitime.
