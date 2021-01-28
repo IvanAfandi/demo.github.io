4afb3ead59d6be608d16a59fdf4833b9101a44a6
Ivan Afandi <ivan@e8growth.com>
2021-01-28 15:14:41 +0700

    add sc demo file

---
+++ app.js
@@ -0,0 +1,12 @@
+var num = Math.floor(Math.random() * 4) +1;
+
+$('#card').wScratchPad({
+    size: 100,          // The size of the brush/scratch.
+    bg: 'image/sc-win.png',  // Background (image path or hex color).
+    fg: 'image/sc-default.png',  // Foreground (image path or hex color).
+    realtime: true,       // Calculates percentage in realitime.
+    scratchDown: null,       // Set scratchDown callback.
+    scratchUp: null,       // Set scratchUp callback.
+    scratchMove: null,       // Set scratcMove callback.
+    cursor: 'pointer' // Set cursor.
+  });
